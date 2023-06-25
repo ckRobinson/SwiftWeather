06e663961b6ba77619c103b702f2c4b2358197e5
Cameron Robinson <cameron@ckrobinson.net>
2023-06-25 10:17:35 -0700

    Began implementing content UI.

--- Swift Weather App/ContentView.swift
+++ Swift Weather App/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel: WeatherDataViewModel = WeatherDataViewModel()
+    var body: some View {
+        
+        ZStack {
+            WeatherBackgroundView()
+            
+            if let weatherData = viewModel.weatherData {
+                WeatherContent()
+                    .padding(.horizontal)
+                    .padding(.top)
+                    .padding(.bottom, 3)
+            }
+        }
+        .navigationTitle("Weather")
+        .onAppear() {
+            viewModel.updateWeatherData()
+        }
+    }
+    
+
+}
+
+struct WeatherContent: View {
+    
+    let weatherData: WeatherModel
+    init() {
+        self.weatherData = WeatherModel.mockData
+    }
+    init(weatherData: WeatherModel) {
+        self.weatherData = weatherData;
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text(weatherData.name)
+                .font(.system(size: 15))
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+                .padding(.leading)
+            
         }
-        .padding()
+        .background(.blue)
+        .cornerRadius(15)
+        .shadow(radius: 1, x: 1, y: 1)
+    }
+}
+
+struct WeatherBackgroundView: View {
+    
+    var body: some View {
+        LinearGradient(colors: [.teal, .blue, .init(red: 0.2, green: 0.2, blue: 1)],
+                       startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
     }
 }
 
