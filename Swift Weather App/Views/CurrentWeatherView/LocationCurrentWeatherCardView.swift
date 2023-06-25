5d657a6557cf224f2d160c4a4eec0819d1a4e8ef
Cameron Robinson <cameron@ckrobinson.net>
2023-06-25 12:44:17 -0700

    Began refactoring weather card.

---
+++ Swift Weather App/Views/CurrentWeatherView/LocationCurrentWeatherCardView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCurrentWeatherCardView.swift
+//  Swift Weather App
+//
+//  Created by Cameron on 6/25/23.
+//
+
+import SwiftUI
+
+struct LocationCurrentWeatherCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationCurrentWeatherCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationCurrentWeatherCardView()
+    }
+}
