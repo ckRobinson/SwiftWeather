0ea575c867f8238438fa5c6116364007f7651141
Cameron Robinson <cameron@ckrobinson.net>
2023-06-25 09:10:49 -0700

    Initial Commit

---
+++ Swift Weather App/Swift_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Weather_AppApp.swift
+//  Swift Weather App
+//
+//  Created by Cameron on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
